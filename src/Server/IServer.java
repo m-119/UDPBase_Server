28b64935f9272b1da1775b1b981b7520fe3cb0fe
MBelyakov <m-119@ya.ru>
2018-07-05 23:14:19 +0300

    commit

--- src/Server/IServer.java
+++ src/Server/IServer.java
@@ -3,16 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Base;
+package Server;
 
 /**
  *
  * @author C
  */
-public class Base {
-        public static void main(String[] args) {
-            
-        System.out.println("Подключение к базе запущено");
-        
-    }
+public interface IServer {
+    
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -3,12 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Base;
+package Server;
 
 /**
  *
  * @author C
  */
-public interface IBase {
-    
+public class Server {
+        public static void main(String[] args) {
+            
+        System.out.println("Подключение к базе запущено");
+        
+    }
 }
