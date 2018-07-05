3ed9c4b3d2c8b6cbb4ad23c8c2144e520e60b46d
MBelyakov <m-119@ya.ru>
2018-07-05 22:57:51 +0300

    commit

---
+++ src/Base/Base.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Base;
+
+/**
+ *
+ * @author C
+ */
+public class Base {
+        public static void main(String[] args) {
+            
+        System.out.println("Подключение к базе запущено");
+        
+    }
+}
