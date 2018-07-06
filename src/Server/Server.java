4b715fa78f674a98c590c99873bfc4061f59930c
MBelyakov <m-119@ya.ru>
2018-07-06 09:15:33 +0300

    непонятно в чём ошибка 01

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -5,14 +5,47 @@
  */
 package Server;
 
+import StringWithID.StringWithID;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  *
  * @author C
  */
 public class Server {
-        public static void main(String[] args) {
-            
-        System.out.println("Подключение к базе запущено");
-        
+
+    public static void main(String args[]) throws Exception {
+
+	System.out.println("Подключение к базе запущено");
+	//буфер отправки
+	byte[] sendData = new byte[1024];
+	//буфер получения
+	byte[] receiveData = new byte[1024];
+
+	DatagramSocket serverSocket = new DatagramSocket(9002);
+
+	while (true) {
+
+	    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+	    serverSocket.receive(receivePacket);
+
+	    //десериализуем
+	    StringWithID swid = StringWithID.deserialize(receivePacket.getData());
+
+	    System.out.println("Получено: " + swid.toString() + " от Клиент #" + swid.toInt());
+	    InetAddress IPAddress = receivePacket.getAddress();
+	    int port = receivePacket.getPort();
+
+	    //обработка
+	    StringWithID NewSWID = new StringWithID(swid.toInt(), swid.toString().toUpperCase());
+
+	    sendData = StringWithID.serialize(NewSWID);
+	    DatagramPacket sendPacket
+		    = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+	    serverSocket.send(sendPacket);
+	}
     }
 }
